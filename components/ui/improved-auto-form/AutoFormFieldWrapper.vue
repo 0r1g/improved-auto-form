9f53a633bc5f75de85c46bebdf85cbe2a5b4c91e
1 <poliakov24ig@gmail.com>
2024-12-05 21:10:17 +0100

    first commit

---
+++ components/ui/improved-auto-form/AutoFormFieldWrapper.vue
@@ -0,0 +1,32 @@
+<!-- AutoFormFieldWrapper.vue -->
+<template>
+  <FormField v-slot="slotProps" :name="fieldName">
+    <FormItem>
+      <AutoFormLabel v-if="!config?.hideLabel" :required="required">
+        {{ config?.label || beautifyObjectName(label ?? fieldName) }}
+      </AutoFormLabel>
+      <FormControl>
+        <slot v-bind="slotProps" />
+      </FormControl>
+      <FormDescription v-if="config?.description">
+        {{ config.description }}
+      </FormDescription>
+      <FormMessage />
+    </FormItem>
+  </FormField>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { FormField, FormItem, FormControl, FormDescription, FormMessage } from '@/components/ui/form'
+import AutoFormLabel from './AutoFormLabel.vue'
+import type { Config, ConfigItem } from './interface'
+import { beautifyObjectName } from './utils'
+
+const props = defineProps<{
+  fieldName: string
+  config?: any
+  required?: boolean
+  label?: string
+}>()
+</script>
